bf7736ef9f0fb0cd091250310d0fed92a69dc91d
WinsEdwards <127224520+WinsEdwards@users.noreply.github.com>
2023-08-07 11:22:16 -0400

    Create ReviewsListUser.vue

---
+++ src/components/ReviewsListUser.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ReviewItem from "../components/ReviewItem.vue"
+import ReviewForm from "../components/ReviewForm.vue"
+// defineProps({
+// reviews: {
+//     type: Array,
+//     required: true
+// }
+// })
+</script>
