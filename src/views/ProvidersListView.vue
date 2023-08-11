bc4ac488021450443563bba2db1a42299a3417cb
maggie weir <mlweir98@gmail.com>
2023-08-11 14:17:12 -0400

    cleaned up file

--- src/views/ProvidersListView.vue
+++ src/views/ProvidersListView.vue
@@ -1,16 +1,7 @@
 <script setup>
-import ProviderListItem from '../components/ProviderListItem.vue';
 import SearchBar from '../components/SearchBar.vue';
-import ReviewForm from '../components/ReviewForm.vue';
-import ReviewsListProvider from '../components/ReviewsListProvider.vue';
-import { store } from '../store';
-import { RouterLink, RouterView } from 'vue-router'
 import ProviderList from '../components/ProviderList.vue'
 
-// functions
-const handleClick = () => {
-    store.getReviewsForProvider()
-}
 </script>
 
 <template>
