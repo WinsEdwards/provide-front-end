f051a1dfdec0108329399119c6f75321f47705eb
Winslow <owedwards@gmail.com>
2023-08-14 19:09:41 -0400

    summary stats calculated, not displaying perfect

--- src/components/ReviewsListProvider.vue
+++ src/components/ReviewsListProvider.vue
@@ -8,6 +8,7 @@ onMounted(() => store.getReviewsForProvider());
 onUpdated(() => {
     store.getReviewsForProvider()
 });
+
 const handleClick = (reviewData) => {
     store.toggleLike(reviewData)
 }
@@ -21,12 +22,32 @@ const checkLiked = (review) => {
         return false
     }
 }
+
+// renaming keys so things print pretty on page
+// const subKeyForQuest = () => {
+//     if (key == "recommended") {return key = "would the reviewer recommend to a friend?:"}
+//     else if (key == "usercomfort") {return key = "did the reviewer feel comfortable?: "}
+//     else if (key == "provcomfort") {return key = "did the provider seem comfortable?"}
+//     else if (key == "staffcomfort") {return key = "did support staff seem comfortable?"}
+//     else if (key == "provknowledge") {return key = "did the provider seem knowledgeable about LGBTQIA2S+ health topics?"}
+//     else if (key == "intake") {return key = "did intake forms include LGBTQIA2S+ inclusive language?"}
+//     else if (key == "misgender") {return key = "did the provider and staff use the reviewers' name and preferred pronouns?"}
+//     else if (key == "bathrooms") {return key = "were gender-neutral bathrooms available?"}
+// }
+
 </script>
 
 <template>
 
     <div class="review-container">
         <div v-if="store.providerReviews.length > 0">
+            <h3>
+                Average ratings for {{ store.currentProvider["first_name"] }} {{ store.currentProvider["last_name"] }}, {{ store.currentProvider["licenses"] }}:
+            </h3>
+                <ul>
+                    <li v-for="(value, key) in store.summaryStats">{{ key }}: {{ value }}</li> 
+                </ul>
+            <br>
             <h3>
                 Reviews for {{ store.currentProvider["first_name"] }} {{ store.currentProvider["last_name"] }}, {{ store.currentProvider["licenses"] }}:
             </h3>
