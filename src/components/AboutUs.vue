d8ecd74971fee94bf6fa96caa89530ffa18007ab
maggie weir <mlweir98@gmail.com>
2023-08-17 15:41:26 -0400

    updated about page and home page

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -18,67 +18,12 @@ const maz_info = {
 </script>
 
 <template>
-<<<<<<< HEAD
-  <div class="provide-explanation">
-
-    <div class="why-provide-img-container">
-      <img src="../assets/images/why-provide-pink-checker.png">
-    </div>
-    
-    <div class="lgbtq-explanation">
-      <h4>We created Provide because we know how hard it can be to locate health care providers who create a safe and affirming environment for LGBTQIA2S+ folks. When we say LGBTQIA2S+, we mean:</h4> 
-=======
   <div class="provide-explanation-container">
-    <div class="provide-explanation">
-      <h2>Why Provide?</h2>
-      <h4>We created Provide because we know how hard it can be to locate health care providers who create a safe and affirming environment for diverse LGBTQIA2S+ folks. When we say LGBTQIA2S+, we mean:</h4> 
->>>>>>> 78e147181b478e0e14323449e4f80d23e74fe1bc
-      <ul>
-        <li>lesbian</li>
-        <li>gay</li>
-        <li>bisexual</li>
-        <li>transgender</li>
-        <li>queer + questioning</li>
-        <li>intersex</li>
-        <li>asexual</li>
-        <li>Two-spirit</li>
-        <li>+ infinite ✨magical✨ expressions of diverse genders and sexualities</li>
-      </ul>
-    </div>
-<<<<<<< HEAD
-    
-    <div class="what-provide-is">
-      <h4>Provide allows you to search and review any provider with an NPI (National Provider Identification) number, which includes any health care provider who is covered by HIPAA (the Health Insurance Portability and Accountability Act) and bills federally funded programs for health services. Basically, any health care provider who bills any kind of health insurance (including Medicare and Medicaid) will have an NPI number, including:</h4>
-=======
-  </div>
-  <div class="provide-explanation-container">
-    <div class="provide-explanation">
-      <h2>How does Provide work?</h2>
-      <h4>Provide allows you to search and review any provider with an NPI (National Provider Identification) number, which includes any health care provider who accepts any kind of health insurance (including Medicare and Medicaid), including:</h4>
->>>>>>> 78e147181b478e0e14323449e4f80d23e74fe1bc
-      <ul>
-        <li>doctors</li>
-        <li>physician assistants</li>
-        <li>nurse practitioners</li>
-        <li>physical and occupational therapists</li>
-        <li>dentists</li>
-        <li>optometrists</li>
-        <li>social workers, counselors and therapists</li>
-        <li>psychiatrists</li>
-        <li>and more!</li>
-      </ul>
-    </div>
-<<<<<<< HEAD
-=======
-  </div>
-  <div class="provide-explanation-container">
-    <div class="provide-explanation">
+    <div class="what-should-I-know">
       <h2>What should I know?</h2>
       <h4>On Provide, you can post and delete your own reviews. You can also read and like others reviews. Any reviews you post are posted anonymously, which means that other users cannot see the username or any other identifying info of the person who posted a given review. On your account page, you can see and delete any review you have written and you can also see any reviews you have liked. To create a new review, search a provider and then fill in your review info and click submit!</h4>
     </div>
-  </div>
-  <div class="provide-explanation-container">
-    <div class="provide-explanation">
+    <div class="future-plans">
       <h2>What are future plans for Provide?</h2>
       <h4>We recognize that individual experiences with a provider are impacted by structural systems of power and oppression such as:</h4>
       <ul>
@@ -96,15 +41,12 @@ const maz_info = {
       </ul>
       <h4> We are actively thinking about ways to encompass these important aspects of experiences with providers that may be helpful for other community members as they make decisions about who they want to see. For now, if it feels comfortable to include, please share in your review whatever feels relevant. Remember that your reviews are seen anonymously, i.e. other users don't see a username or any other identifying information attached to the reviews you post on Provide.</h4>
     </div>
->>>>>>> 78e147181b478e0e14323449e4f80d23e74fe1bc
   </div>
-
-  <div class="about-us-container">
-
-    <div class="about-us-img-container">
+  
+  <div class="about-us-img-container">
       <img class="about-us-img" src="../assets/images/about-us-yellow.png" alt="big, dark green bubble letter text that says about us">
-    </div>
-
+  </div>
+  <div class="about-us-container">
     <div class="winslow">
 
       <div class="about-us-child-1">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,6 +12,45 @@ import '../assets/searchbar.css'
       <SearchBar class="home-view-search-bar"></SearchBar>
     </div>
     
+    <div class="provide-explanation">
+      <div class="why-provide-img-container">
+        <img src="../assets/images/why-provide-pink-checker.png">
+      </div>
+    
+      <!-- <div class="laptop-search-img"><img src="../assets/images/laptop-use.jpg"></div> -->
+
+      <div class="lgbtq-explanation">
+        <h4>We created Provide because we know how hard it can be to locate health care providers who create a safe and affirming environment for LGBTQIA2S+ folks. When we say LGBTQIA2S+, we mean:</h4> 
+        <ul>
+          <li>lesbian</li>
+          <li>gay</li>
+          <li>bisexual</li>
+          <li>transgender</li>
+          <li>queer + questioning</li>
+          <li>intersex</li>
+          <li>asexual</li>
+          <li>Two-spirit</li>
+          <li>+ infinite ✨magical✨ expressions of diverse genders and sexualities</li>
+        </ul>
+      </div>
+      
+      <div class="what-provide-is">
+        <h2>How does Provide work?</h2>
+        <h4>Provide allows you to search and review any provider with an NPI (National Provider Identification) number, which includes any health care provider who accepts any kind of health insurance (including Medicare and Medicaid), including:</h4>
+        <ul>
+          <li>doctors</li>
+          <li>physician assistants</li>
+          <li>nurse practitioners</li>
+          <li>physical and occupational therapists</li>
+          <li>dentists</li>
+          <li>optometrists</li>
+          <li>social workers, counselors and therapists</li>
+          <li>psychiatrists</li>
+          <li>and more!</li>
+        </ul>
+      </div>
+      
+    </div>
   </main>
 </template>
 
